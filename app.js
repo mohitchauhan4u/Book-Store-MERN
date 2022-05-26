6156e2977a0c2e6642f38725eae78e4e53e762e9
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-26 21:12:13 +0530

    added express and working on backend

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello From Book Store!");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+//middleware
+
+app.use("/", (req, res, next) => {
+  res.send("Connected");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://mohitchauhan4u:9306138730@cluster0.loauf.mongodb.net/bookstore"
+  )
+  .then(() => console.log("Connected to database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log("ERROE ", err));
