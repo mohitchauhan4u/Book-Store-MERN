342e869778d3ba28f1db06c2b0ff6ed44ec75d82
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-27 21:57:07 +0530

    created getById

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -14,6 +14,20 @@ const getAllBook = async (req, res, next) => {
   return res.status(200).json({ books });
 };
 
+const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findById(id);
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "No Book found" });
+  }
+  return res.status(200).json({ book });
+};
+
 const addBook = async (req, res, next) => {
   const { name, author, description, price, available } = req.body;
   let book;
@@ -31,3 +45,4 @@ const addBook = async (req, res, next) => {
 
 exports.getAllBook = getAllBook;
 exports.addBook = addBook;
+exports.getById = getById;
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -4,5 +4,6 @@ const booksController = require("../controllers/book-controller");
 
 router.get("/", booksController.getAllBook);
 router.post("/", booksController.addBook);
+router.get("/:id", booksController.getById);
 
 module.exports = router;
