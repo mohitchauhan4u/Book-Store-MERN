ffae71eec0efafdb446b2601f3e14ef4afb52bfa
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-29 10:28:53 +0530

    creating book componenet and

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const router = require("./routes/book-routes");
 
 //middleware
 app.use(express.json());
+app.use(cors());
 app.use("/books", router);
 
 mongoose
--- book-store/src/components/Book/Books.jsx
+++ book-store/src/components/Book/Books.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const URL = "http://localhost:5000/books";
+
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
 
 const Books = () => {
+  const [books, setbooks] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setbooks(data));
+  });
+  console.log(books);
   return <div>Books</div>;
 };
 
