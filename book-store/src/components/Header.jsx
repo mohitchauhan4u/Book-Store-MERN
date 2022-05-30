51dc8e0fadb0e4f9d7145f575050f26ed413f1ca
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-30 15:09:27 +0530

    craeted Home page component

--- book-store/src/components/Header.jsx
+++ book-store/src/components/Header.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, Tabs, Tab } from "@mui/material";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import { NavLink } from "react-router-dom";
 const Header = () => {
-  const [value, setvalue] = useState();
+  const [value, setValue] = useState();
   return (
     <div>
       <AppBar sx={{ backgroundColor: "#232F3D" }} position="sticky">
         <Toolbar>
-          <Typography>
-            <LibraryBooksOutlinedIcon />
-          </Typography>
+          <NavLink to="/" style={{ color: "white" }}>
+            <Typography>
+              <LibraryBooksOutlinedIcon />
+            </Typography>
+          </NavLink>
           <Tabs
             sx={{ ml: "auto" }}
             textColor="inherit"
             indicatorColor="primary"
             value={value}
-            onChange={(e, val) => setvalue(val)}
+            onChange={(e, val) => setValue(val)}
           >
-            <Tab LinkComponent={NavLink} to="/add" label="Add Product" />
+            <Tab LinkComponent={NavLink} to="/add" label="Add product" />
             <Tab LinkComponent={NavLink} to="/books" label="Books" />
             <Tab LinkComponent={NavLink} to="/about" label="About Us" />
           </Tabs>
