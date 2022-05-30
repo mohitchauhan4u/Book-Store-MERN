5281619d5f2c2c7666c71d66df53db178e240bd7
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-30 14:16:13 +0530

    craeting update book (Bookdetails) component

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import About from "./components/About";
 import Books from "./components/Book/Books";
+import BookDetails from "./components/Book/BookDetails";
 
 const App = () => {
   return (
@@ -16,8 +17,9 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/add" element={<AddBook />} />
-          <Route exact path="/books" element={<Books />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/books" element={<Books />} />
+          <Route exact path="/books/:id" element={<BookDetails />} />
         </Routes>
       </main>
     </>
--- book-store/src/components/Book/BookDetails.jsx
+++ book-store/src/components/Book/BookDetails.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const BookDetails = () => {
-  return <div>BookDetails</div>;
+  const id = useParams().id;
+  console.log(id);
+  useEffect(() => {
+    const fetchHandler = async () => {
+      await axios
+        .get(`http://localhost:5000/books/${id}`)
+        .then((res) => console.log(res.data));
+    };
+    fetchHandler();
+  }, [id]);
+
+  return <div></div>;
 };
 
 export default BookDetails;
