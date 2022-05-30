7daeef1c5d260eaebc186272a8ea387840e7a547
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-30 15:12:38 +0530

    added styling

--- book-store/src/components/Book/Books.jsx
+++ book-store/src/components/Book/Books.jsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from "react";
+import "./Book.css";
 import axios from "axios";
 import Book from "./Book";
-import "./Book.css";
 const URL = "http://localhost:5000/books";
-
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
 };
-
 const Books = () => {
-  const [books, setbooks] = useState();
+  const [books, setBooks] = useState();
   useEffect(() => {
-    fetchHandler().then((data) => setbooks(data.books));
+    fetchHandler().then((data) => setBooks(data.books));
   }, []);
   console.log(books);
   return (
     <div>
       <ul>
         {books &&
-          books.map((book, key) => (
-            <li key={key} className="book">
+          books.map((book, i) => (
+            <li key={i}>
               <Book book={book} />
             </li>
           ))}
